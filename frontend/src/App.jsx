b1e47a36a60814c43fbaa96f1198181baddd2390
Dongchu21 <dongchu21@Mac.home>
2026-07-23 18:51:09 +0200

    update project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-3xl font-bold text-teal-500">Welcome to EMS</div>
+    </>
+  )
+}
+
+export default App
